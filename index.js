4d06b054676d62a38e3a584e2a6276eb47829ca9
pratheep-sr <pratheepjenish75@gmail.com>
2026-01-10 17:08:57 +0530

    image

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
+const express = require("express");
+const jsonServer = require("json-server");
+
+const app = express();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-server.use(middlewares);
-server.use(router);
+// Serve Static Files
+app.use("/public", express.static("public"));
 
-server.listen(port);
+app.use(middlewares);
+app.use(router);
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
